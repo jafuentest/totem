8bf68e917257afacefad88584ac378f19b2b315f
Juan Fuentes <jn.fuentest@gmail.com>
2015-04-08 00:46:46 -0430

    Incluir el menu lateral en el MasterPage principal A partir de ahora se manejará unicamente este Master Page y el menú principal se manejará mediante un XML único para todo el proyecto. Queda de parte de cada módulo la responsabilidad de eliminar su respectivo MasterPage y actualizar sus páginas para que funcionen bajo el nuevo formato.

--- GUI/Master/MasterPage.master.cs
+++ GUI/Master/MasterPage.master.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Xml;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+	private string idModulo;
+	private Dictionary<string, string> opcionesDelMenu = new Dictionary<string, string>();
 
-    }
+	public string IdModulo
+	{
+		get { return idModulo; }
+		set { idModulo = value; }
+	}
+	public Dictionary<string, string> OpcionesDelMenu
+	{
+		get { return opcionesDelMenu; }
+		set { opcionesDelMenu = value; }
+	}
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		XmlDocument doc = new XmlDocument();
+		doc.Load(Server.MapPath("~/GUI/Master/menuLateral.xml"));
+
+		foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			foreach (XmlNode subNode in node.ChildNodes)
+				if (subNode.Attributes["id"].InnerText.Equals(IdModulo))
+				{
+					OpcionesDelMenu[node.Attributes["nombre"].InnerText] = node.Attributes["link"].InnerText;
+					break;
+				}
+	}
 }
