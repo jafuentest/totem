41ba9d272f7ce4315e4907cb361ed3a35ab98386
Scherylu <scheryluci@gmail.com>
2015-04-13 14:27:02 -0430

    Modulo 3

--- GUI/Modulo3/AgregarInvolucrado.aspx.cs
+++ GUI/Modulo3/AgregarInvolucrado.aspx.cs
@@ -10,6 +10,25 @@ public partial class GUI_Modulo3_Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         ((MasterPage)Page.Master).IdModulo = "3";
+      
+          if (Request.Cookies["userInfo"] != null)
+         {
+            if (Server.HtmlEncode(Request.Cookies["userInfo"]["usuario"]) != "" &&
+                Server.HtmlEncode(Request.Cookies["userInfo"]["clave"]) != "")
+            {
+                ((MasterPage)Page.Master).ShowDiv = true;
+            }
+            else
+            {
+                ((MasterPage)Page.Master).MostrarMenuLateral = false;
+                ((MasterPage)Page.Master).ShowDiv = false;
+            }
+
+        }
+        else
+        {
+            Response.Redirect("../Modulo1/M1_login.aspx");
+        }   
     }
 
     protected void proyecto1_SelectedIndexChanged(object sender, EventArgs e)
