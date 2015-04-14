bf11435c1839d2652c7761f2b5e47740545de174
tjsearsz <tjsearsz@gmail.com>
2015-04-14 12:14:16 -0430

    Modulo 6 punto 1
    Agregado menu de arriba

--- GUI/Modulo6/ReporteActores.aspx.cs
+++ GUI/Modulo6/ReporteActores.aspx.cs
@@ -5,5 +5,6 @@ public partial class GUI_Modulo6_ReporteActores : System.Web.UI.Page
 	protected void Page_Load(object sender, EventArgs e)
 	{
 		((MasterPage)Page.Master).IdModulo = "6";
+        ((MasterPage)Page.Master).ShowDiv = true;
 	}
 }
