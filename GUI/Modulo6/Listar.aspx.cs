d1afe359e2b1ac638f7a25f13574d05894f0aebd
Juan Fuentes <jn.fuentest@gmail.com>
2015-04-15 00:39:29 -0430

    Redireccion al módulo de login..........

--- GUI/Modulo6/Listar.aspx.cs
+++ GUI/Modulo6/Listar.aspx.cs
@@ -5,6 +5,20 @@ public partial class GUI_Modulo6_Listar : System.Web.UI.Page
 	protected void Page_Load(object sender, EventArgs e)
 	{
 		((MasterPage)Page.Master).IdModulo = "6";
-        ((MasterPage)Page.Master).ShowDiv = true;
+		
+		if (Request.Cookies["userInfo"].Equals(null))
+		{
+			Response.Redirect("~/GUI/Modulo1/M1_login.aspx");
+		}
+		else if (Server.HtmlEncode(Request.Cookies["userInfo"]["usuario"]) != "" &&
+				Server.HtmlEncode(Request.Cookies["userInfo"]["clave"]) != "")
+		{
+			((MasterPage)Page.Master).ShowDiv = true;
+		}
+		else
+		{
+			((MasterPage)Page.Master).MostrarMenuLateral = false;
+			((MasterPage)Page.Master).ShowDiv = false;
+		}
 	}
 }
