ff6d346cf03909e0187f933b57421f7fe314e79d
Scherylu <scheryluci@gmail.com>
2015-05-22 16:29:16 -0430

    Modulo 7
    Creación de clase usuario

---
+++ src/DominioTotem/Usuario.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DominioTotem
+{
+    class Usuario
+    {
+        public string username { get; set; }
+        public string clave { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string rol { get; set; }
+        public string correo { get; set; }
+        public string preguntaSeguridad { get; set; }
+        public string respuestaSeguridad { get; set; }
+        public string cargo { get; set; }
+
+
+        public Usuario() { 
+        
+        }
+
+
+        //Por implementar
+        public Boolean RegistrarUsuario() {
+             
+            return true;
+        }
+
+        public Boolean ModificarUsuario()
+        {
+
+            return true;
+        }
+
+        public Boolean ModificarUsuario()
+        {
+
+            return true;
+        }
+
+        public Boolean ValidarCorreo()
+        {
+
+            return true;
+        }
+
+        public Boolean ValidarUsuario()
+        {
+
+            return true;
+        }
+
+        public Boolean RegistrarUsuario()
+        {
+
+            return true;
+        }
+
+        public List<String> obtenerCargoDeUsuarios()
+        {
+            List<String> listaCargo = new List<String>();
+
+            return listaCargo;
+        }
+
+           public List<Usuario> filtrarUsuariosPorCargo(String cargo)
+        {
+
+            List<Usuario> listaUsuario = new List<Usuario>();
+
+            return listaUsuario;
+        }
+
+           public Usuario consultarDatosDeUsuario(String nombre, String apellido, String cargo)
+        {
+            Usuario usuario = new Usuario();
+
+            return usuario;
+        }
+
+        
+
+        
+    
+    }
+}
