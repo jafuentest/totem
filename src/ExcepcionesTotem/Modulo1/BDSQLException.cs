ae62070091a02455f538e54df517c0bcd5079130
samgville <sammyvillegas@gmail.com>
2015-05-23 16:51:53 -0430

    Agregar Excepciones
    Agregada Excepcion de BDSQL y de Intentos Errados

---
+++ src/ExcepcionesTotem/Modulo1/BDSQLException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo1
+{
+    public class IntentosErradosException : ExceptionTotem
+    {
+
+        public IntentosErradosException()
+            : base()
+        { }
+
+        public IntentosErradosException(string message)
+            : base(message)
+        {
+        }
+
+        public IntentosErradosException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+
+}
+
