1309a7be656c78791317f03a95e5fc0d47a97e2d
sbernal93 <santiagobernal93@gmail.com>
2015-05-23 12:23:56 -0430

    Creacion de clases de pruebas unitarias modulo 1

---
+++ src/PruebasUnitariasTotem/Modulo1/PruebaLogicaLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PruebasUnitariasTotem.Modulo1
+{
+    class PruebaLogicaLogin
+    {
+    }
+}
