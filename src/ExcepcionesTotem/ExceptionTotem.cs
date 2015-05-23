602d8612a503f7373690ca0e20bc3e96af6226c0
samgville <sammyvillegas@gmail.com>
2015-05-23 13:31:35 -0430

    Exceptiones
    Creada ExceptionTotem, Creada LoginErradoException

---
+++ src/ExcepcionesTotem/ExceptionTotem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem
+{
+    public class ExceptionTotem : Exception
+    {
+        private String _Codigo;
+        private String _Mensaje;
+      
+        public ExceptionTotem()
+        {
+
+        }
+
+    }
+}
---
+++ src/ExcepcionesTotem/Modulo1/LoginErradoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo1
+{
+    public class LoginErradoException : Exception
+    {
+
+
+    }
+}
