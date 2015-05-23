e7d38f769dfa06df9bc7d162687bf25f1e54a96a
samgville <sammyvillegas@gmail.com>
2015-05-23 15:24:14 -0430

    Pruebas de Excepciones
    Manejo de Pruebas con Exceptions

---
+++ src/ExcepcionesTotem/Modulo1/EmailErradoException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo1
+{
+    public class EmailErradoException : ExceptionTotem
+    {
+
+        public EmailErradoException()
+            : base()
+        { }
+
+        public EmailErradoException(string message)
+            : base(message)
+        {
+        }
+
+        public EmailErradoException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+
+}
+
