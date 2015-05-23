2c3f93350ecc699114e94ebc2cda68e91acac70c
kakikeunda <ceunda92@gmail.com>
2015-05-23 15:06:13 -0430

    Clases de modulo 3
    Dominio, BD y algo de logica

---
+++ src/DominioTotem/ListaInvolucradoUsuario.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioTotem
+{
+    public class ListaInvolucradoUsuario
+    {
+        private Proyecto proyecto;
+        private List<Usuario> lista;
+
+        #region Constructores
+        public ListaInvolucradoUsuario()
+        {
+            proyecto = null;
+            lista = new List<Usuario>();
+        }
+        public ListaInvolucradoUsuario(Proyecto p)
+        {
+            proyecto = p;
+            lista = new List<Usuario>();
+        }
+        public ListaInvolucradoUsuario(List<Usuario> laLista, Proyecto p)
+        {
+            proyecto = p;
+            lista = laLista;
+        }
+        #endregion
+
+        #region Getters y Setters
+        public Proyecto Proyecto
+        {
+            get 
+            { 
+                return proyecto; 
+            }
+            set 
+            { 
+                proyecto = value; 
+            }
+        }
+        public List<Usuario> Lista
+        {
+            get 
+            { 
+                return lista; 
+            }
+            set 
+            { 
+                lista = value; 
+            }
+        }
+        #endregion
+
+        public ListaInvolucradoUsuario obtenerUsuariosInvolucradosProyecto(Proyecto elProyecto)
+        {
+            throw new NotImplementedException();
+        }
+        public bool eliminarUsuarioDeProyecto(Usuario u)
+        {
+            return lista.Remove(u);
+            
+            //throw new NotImplementedException();
+        }
+        public bool agregarUsuarioAProyecto(Usuario u)
+        {
+            lista.Add(u);
+
+            if (lista.Last() == u)
+                return true;
+            else
+                return false;
+        }
+        public bool agregarListaEnBD(ListaInvolucradoUsuario laLista)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
