e039bb228f7f191647bd9cdabce16bb3f47cc573
samgville <sammyvillegas@gmail.com>
2015-05-23 16:53:31 -0430

    Errores de Compilado corregidos

---
+++ src/ExcepcionesTotem/Modulo1/BDSQLException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo1
+{
+    public class BDSQLException : ExceptionTotem
+    {
+
+        public BDSQLException()
+            : base()
+        { }
+
+        public BDSQLException(string message)
+            : base(message)
+        {
+        }
+
+        public BDSQLException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+
+}
+
