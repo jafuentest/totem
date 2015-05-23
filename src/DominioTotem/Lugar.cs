4236babeac1cba65ab9325f5fc1f9068b9ba02e9
alvarodgarridoc <maskmer321@gmail.com>
2015-05-23 14:38:36 -0430

    Agregándole comentarios a las clases
    Se le agregaron comentarios a las clases del Dominio de Módulo 2

--- src/DominioTotem/Lugar.cs
+++ src/DominioTotem/Lugar.cs
@@ -11,8 +11,8 @@ namespace DominioTotem
        #region Atributos
 
        private int idLugar;      
-       private String nombreLugar;       
-       private String tipoLugar;
+       private string nombreLugar;       
+       private string tipoLugar;
        private int fkLugar;
 
        #endregion
@@ -23,12 +23,12 @@ namespace DominioTotem
            get { return idLugar; }
            set { idLugar = value; }
        }
-       public String NombreLugar
+       public string NombreLugar
        {
            get { return nombreLugar; }
            set { nombreLugar = value; }
        }
-       public String TipoLugar
+       public string TipoLugar
        {
            get { return tipoLugar; }
            set { tipoLugar = value; }
@@ -38,6 +38,64 @@ namespace DominioTotem
            get { return fkLugar; }
            set { fkLugar = value; }
        }
+       #endregion
+
+       #region Constructores
+
+
+       /// <summary>
+       /// Constructor de la clase Lugar
+       /// </summary>
+       public Lugar() 
+       {
+           IdLugar = 0;
+           NombreLugar = string.Empty;
+           TipoLugar = string.Empty;
+           FkLugar = 0; 
+       }
+
+       /// <summary>
+       /// Constructor de la Clase Lugar
+       /// </summary>
+       /// <param name="nombre">Nombre del Lugar</param>
+       public Lugar(int id,string nombre) 
+       {
+           IdLugar = id;
+           NombreLugar = nombre;
+           TipoLugar = string.Empty;
+           FkLugar = 0; 
+       }
+
+       /// <summary>
+       /// Constructor de la Clase Lugar
+       /// </summary>
+       /// <param name="id">Identificador del Lugar</param>
+       /// <param name="nombre">Nombre del Lugar</param>
+       /// <param name="tipoLugar">Tipo de Lugar (país,estado,ciudad o dirección)</param>
+       public Lugar(int id, string nombre, string tipoLugar) 
+       {
+           IdLugar = id;
+           NombreLugar = nombre;
+           TipoLugar = tipoLugar;
+           FkLugar = 0; 
+       }
+
+
+       /// <summary>
+       /// Constructor de la Clase Lugar
+       /// </summary>
+       /// <param name="id">Identificador del Lugar</param>
+       /// <param name="nombre">Nombre del Lugar</param>
+       /// <param name="tipoLugar">Tipo de Lugar (país,estado,ciudad o dirección)</param>
+       /// <param name="fkLugar">Número identificador que engloba este lugar</param>
+       public Lugar(int id, string nombre, string tipoLugar, int fkLugar) 
+       {
+           IdLugar = id;
+           NombreLugar = nombre;
+           TipoLugar = tipoLugar;
+           FkLugar = fkLugar; 
+       }
+
        #endregion
 
    }
