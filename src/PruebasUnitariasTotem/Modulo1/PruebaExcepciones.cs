70aa819b7dcb1af6297a82da1a3afe055eefcc8b
samgville <sammyvillegas@gmail.com>
2015-05-23 14:37:13 -0430

    Cambios a LogicaLogin y a PruebasExceptions
    Cambiar parametro de metodo login a un objeto Usuario, Prueba de
    LoginErradoException probada.

---
+++ src/PruebasUnitariasTotem/Modulo1/PruebaExcepciones.cs
@@ -0,0 +1,50 @@
+﻿using DominioTotem;
+using ExcepcionesTotem.Modulo1;
+using LogicaNegociosTotem.Modulo1;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PruebasUnitariasTotem.Modulo1
+{
+    [TestFixture]
+    public class PruebaExcepciones
+    {
+        [SetUp]
+        public void init()
+        {
+
+        }
+
+        [Test]
+
+        public void PruebaLoginErradoException()
+        {
+            try
+            {
+                var logica = new LogicaLogin();
+                var usuario = new Usuario();
+                usuario.clave = "";
+                usuario.username = "";
+                logica.Login(usuario);
+                Assert.Fail("Una excepcion se ha debido de lanzar");
+            }
+            catch (LoginErradoException loginErradoException)
+            {
+                Assert.AreEqual("La clave o el usuario son incorrectos", loginErradoException.Message);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(
+                string.Format("Unexpected exception of type {0} caught: {1}",
+                           e.GetType(), e.Message)
+                );
+
+
+            }
+
+        }
+    }
+}
