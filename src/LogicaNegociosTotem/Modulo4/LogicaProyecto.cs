fdd18213b115b1826249a0670e70ce3dd3fd58f7
croix000 <plasenciajohan@hotmail.com>
2015-05-26 10:58:01 -0430

    Logica Proyecto
    Creacion de la clase LogicaProyecto en LogicaNegociosTotem

---
+++ src/LogicaNegociosTotem/Modulo4/LogicaProyecto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogicaNegociosTotem.Modulo4
+{
+    /// <summary>
+    /// Clase que maneja la logica de los proyectos
+    /// </summary>
+    public class LogicaProyecto
+    {
+        //Clase que maneja proyecto en la Base de Datos
+        private DatosTotem.Modulo4.BDProyecto bdProyecto;
+
+        #region metodos
+
+
+        /// <summary>
+        /// Metodo para Agregar un proyecto en BD
+        /// </summary>
+        /// <param name="proyecto">proyecto a crear
+        /// <returns>Retorna True si se crea, de lo contrario genera
+        /// una exception(CodigoRepetido)</returns>
+        public bool CrearProyecto(DominioTotem.Proyecto proyecto)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Metodo para consultar un proyecto en BD
+        /// </summary>
+        /// <param name="codigo">codigo del proyecto a consultar
+        /// <returns>Devuelve como resultado un proyecto en caso 
+        /// contrario devuelve null</returns>
+        /// 
+        public DominioTotem.Proyecto ConsultarProyecto(String codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Metodo para Modificar un proyecto en BD
+        /// </summary>
+        /// <param name="proyecto">proyecto a crear
+        /// <returns>Devuelve True si lo modifica, de lo contrario devuelve false</returns>
+        public bool ModificarProyecto(DominioTotem.Proyecto proyecto)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
