31e500b465ab14ca2f825345f561de008d4f3e53
tjsearsz <tjsearsz@gmail.com>
2015-05-25 23:20:11 -0430

    Cambio de cookies a variables sesion

--- src/Interfaz/src/GUI/Modulo6/Listar.aspx.cs
+++ src/Interfaz/src/GUI/Modulo6/Listar.aspx.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Web;
 
 public partial class GUI_Modulo6_Listar : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
 		((MasterPage)Page.Master).IdModulo = "6";
-		((MasterPage)Page.Master).ShowDiv = true;
-		String success = Request.QueryString["success"];
+        
+        DominioTotem.Usuario user = HttpContext.Current.Session["Credenciales"] as DominioTotem.Usuario;
+        if (user != null)
+        {
+            if (user.username != "" &&
+                user.clave != "")
+            {
+                ((MasterPage)Page.Master).ShowDiv = true;
+            }
+            else
+            {
+                ((MasterPage)Page.Master).MostrarMenuLateral = false;
+                ((MasterPage)Page.Master).ShowDiv = false;
+            }
 
-		if (Request.Cookies["userInfo"] == null)
-		{
-			Response.Redirect("~/GUI/Modulo1/M1_login.aspx");
-		}
+        }
+        else
+        {
+            Response.Redirect("../Modulo1/M1_login.aspx");
+        }
 
 		switch (success)
 		{
--- src/Interfaz/src/GUI/Modulo6/Modificar.aspx.cs
+++ src/Interfaz/src/GUI/Modulo6/Modificar.aspx.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.Web;
 
 public partial class GUI_Modulo6_Modificar : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
 		((MasterPage)Page.Master).IdModulo = "6";
-		((MasterPage)Page.Master).ShowDiv = true;
+        
+        DominioTotem.Usuario user = HttpContext.Current.Session["Credenciales"] as DominioTotem.Usuario;
+        if (user != null)
+        {
+            if (user.username != "" &&
+                user.clave != "")
+            {
+                ((MasterPage)Page.Master).ShowDiv = true;
+            }
+            else
+            {
+                ((MasterPage)Page.Master).MostrarMenuLateral = false;
+                ((MasterPage)Page.Master).ShowDiv = false;
+            }
 
-		if (Request.Cookies["userInfo"] == null)
-		{
-			Response.Redirect("~/GUI/Modulo1/M1_login.aspx");
-		}
+        }
+        else
+        {
+            Response.Redirect("../Modulo1/M1_login.aspx");
+        }
 	}
 }
