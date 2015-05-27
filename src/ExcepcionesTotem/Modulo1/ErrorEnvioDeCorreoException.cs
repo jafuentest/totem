fcf6c2a2cd288cb45e2274153cfdad8619887297
sbernal93 <santiagobernal93@gmail.com>
2015-05-27 13:17:34 -0430

    Excepcion Nueva

---
+++ src/ExcepcionesTotem/Modulo1/ErrorEnvioDeCorreoException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo1
+{
+    public class ErrorEnvioDeCorreoException : ExceptionTotem
+    {
+        public ErrorEnvioDeCorreoException()
+            : base()
+        { }
+
+        public ErrorEnvioDeCorreoException(string message)
+            : base(message)
+        {
+        }
+
+        public ErrorEnvioDeCorreoException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public ErrorEnvioDeCorreoException(string codigo, string message, Exception inner)
+            : base(codigo, message, inner)
+        {
+        }
+    }
+}
