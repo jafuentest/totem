fc0af8f5b73e97d4e93b15793ef65a54e8e4eb8b
César R. Contreras <c1sar90@GMAIL.COM>
2015-05-28 11:42:49 -0430

    M8 consultar

---
+++ src/DominioTotem/ResponseObject/ResponseDataMinuta.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DominioTotem.ResponseObject
+{
+    public class ResponseDataMinuta
+    {
+        public int Draw { get; set; }
+
+        public int RecordsTotal { get; set; }
+
+        public int RecordsFiltered { get; set; }
+
+        public IEnumerable<string[]> Data { get; set; }
+    }
+}
