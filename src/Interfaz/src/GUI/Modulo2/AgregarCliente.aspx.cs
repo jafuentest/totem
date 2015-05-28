990a0c2239e93e1355c21be86dda8b3e96caab9c
Minervavr <minerva_veru@hotmail.com>
2015-05-28 11:22:31 -0430

    Agregar Cliente Interfaz

--- src/Interfaz/src/GUI/Modulo2/AgregarCliente.aspx.cs
+++ src/Interfaz/src/GUI/Modulo2/AgregarCliente.aspx.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using LogicaNegociosTotem.Modulo2;
+using System.Web.UI.HtmlControls;
 using DominioTotem;
-using ExcepcionesTotem;
+using LogicaNegociosTotem.Modulo2;
+using ExcepcionesTotem.Modulo2; 
 
 public partial class GUI_Modulo2_AgregarCliente : System.Web.UI.Page
 {
@@ -51,11 +52,19 @@ public partial class GUI_Modulo2_AgregarCliente : System.Web.UI.Page
              
 
             clienteNatural = new ClienteNatural();
-            clienteNatural.Nat_Nombre = TextNombre.Value;
-             clienteNatural.Nat_Apellido= TextApellido.Value;
-            clienteNatural.Nat_Correo = TextCorreo.Value;
-         //    clienteNatural.Nat_Pais= TextPais.;
-        //   clienteNatural.Nat_Ciudad = Convert.ToInt32(DropDownListCiudad.SelectedItem.Value);
+            string nombre = nombreNatural.Value;
+            string apellido = apellidoNatural.Value;
+            string correo = correoNatural.Value;
+            string pais = paisNatural.InnerText;
+            string estado = estadoNatural.InnerText;
+            string ciudad = ciudadNatural.InnerText;
+            string direccion = direccionNatural.Value;
+            string codigoPostal = codigopostalNatural.Value;
+            string telefono = telefonoNatural.Value;
+    
+        
+
+
 
 
             return clienteNatural;
@@ -63,57 +72,132 @@ public partial class GUI_Modulo2_AgregarCliente : System.Web.UI.Page
 
 
 
+         /// <summary>
+         /// Método que extrae la información de los paises para el
+         /// combo
+         /// </summary>
+         private void LlenarPaises()
+         {
+             try
+             {
+                 LogicaLugar logica = new LogicaLugar();
+                 List<Lugar> paises = logica.LlenarComboPaises();
+                 ListItem itemPais;
+
+                 this.comboPais.Items.Clear();
+
+                 itemPais = new ListItem();
+                 itemPais.Text = "Seleccione un país";
+                 itemPais.Value = "0";
+                 this.comboPais.Items.Add(itemPais);
+
+                 foreach (Lugar objetoPais in paises)
+                 {
+                     itemPais = new ListItem();
+                     itemPais.Text = objetoPais.NombreLugar;
+                     itemPais.Value = objetoPais.IdLugar.ToString();
+                     this.comboPais.Items.Add(itemPais);
+                 }
+             }
+             catch (ClienteDatosException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa de datos</div>";
+             }
+             catch (ClienteLogicaException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa lógica</div>";
+             }
+
+         }
 
          /// <summary>
-        /// TextBoxNombre control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBoxNombre;
-        
+         /// Método que extrae la información de los 
+         /// estados para el combo
+         /// </summary>
+         private void LlenarEstados(int idPais)
+         {
+             try
+             {
+                 LogicaLugar logica = new LogicaLugar();
+                 List<Lugar> estados = logica.LlenarComboEstados(idPais);
+                 ListItem itemEstado;
+
+                 this.comboEstado.Items.Clear();
+
+                 itemEstado = new ListItem();
+                 itemEstado.Text = "Seleccione Estado";
+                 itemEstado.Value = "0";
+                 this.comboEstado.Items.Add(itemEstado);
+
+                 foreach (Lugar objetoEstado in estados)
+                 {
+                     itemEstado = new ListItem();
+                     itemEstado.Text = objetoEstado.NombreLugar;
+                     itemEstado.Value = objetoEstado.IdLugar.ToString();
+                     this.comboEstado.Items.Add(itemEstado);
+                 }
+
+             }
+             catch (ClienteDatosException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa de datos</div>";
+             }
+             catch (ClienteLogicaException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa lógica</div>";
+             }
+         }
+
+
          /// <summary>
-        /// TextBoxNombre control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBoxApellido;
-        
+         /// Método que extrae la información de las 
+         /// ciudades para el combo
+         /// </summary>
+         private void LlenarCiudades(int idEstado)
+         {
+             try
+             {
+                 LogicaLugar logica = new LogicaLugar();
+                 List<Lugar> ciudades = logica.LlenarComboCiudades(idEstado);
+                 ListItem itemCiudad;
 
+                 this.comboEstado.Items.Clear();
 
-        /// <summary>
-        /// TextBoxNombre control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBoxCorreo;
-        
-       /// <summary>
-        /// TextBoxNombre control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBoxPais;
-        
+                 itemCiudad = new ListItem();
+                 itemCiudad.Text = "Seleccione Estado";
+                 itemCiudad.Value = "0";
+                 this.comboCiudad.Items.Add(itemCiudad);
 
-       /// <summary>
-        /// TextBoxNombre control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox DropDownListCiudad;
-        
+                 foreach (Lugar objetoCiudad in ciudades)
+                 {
+                     itemCiudad = new ListItem();
+                     itemCiudad.Text = objetoCiudad.NombreLugar;
+                     itemCiudad.Value = objetoCiudad.IdLugar.ToString();
+                     this.comboEstado.Items.Add(itemCiudad);
+                 }
 
-
+             }
+             catch (ClienteDatosException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa de datos</div>";
+             }
+             catch (ClienteLogicaException e)
+             {
+                 alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                 alert.Attributes["role"] = "alert";
+                 alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error en la capa lógica</div>";
+             }
+         }
 
 
 
