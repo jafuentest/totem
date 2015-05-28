abaeb40ddaffe8c749d9d23b9ae7ffc03f4e1d96
Minervavr <minerva_veru@hotmail.com>
2015-05-27 19:38:55 -0430

    Prueba Logica de Consultar Contacto

---
+++ src/LogicaNegociosTotem/Modulo2/LogicaContacto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DominioTotem;
+using DatosTotem.Modulo2;
+using ExcepcionesTotem.Modulo2;
+namespace LogicaNegociosTotem.Modulo2
+{
+    public class LogicaContacto
+    {
+        private BDContacto baseDeDatosContacto; 
+       /// <summary>
+       /// Constructor de la Clase LogicaContacto
+       /// </summary>
+       public LogicaContacto() 
+       {
+           baseDeDatosContacto = new BDContacto(); 
+       }
+
+       
+
+       /// <summary>
+       /// Método que consulta los datos de un contacto según su id
+       /// </summary>
+       /// <param name="clienteJuridico">Información del Contacto</param>
+       /// <returns>Retorna el objeto de tipo Contacto, null si el objeto no existe</returns>
+       public Contacto ConsultarDatosDeContacto(int identificador)
+       {
+           Contacto contacto = new Contacto(identificador);
+
+           return baseDeDatosContacto.ConsultarDatosDeContacto(identificador); 
+       }
+
+
+    }
+}
---
+++ src/PruebasUnitariasTotem/Modulo2/PruebaLogicaContacto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using DominioTotem;
+using LogicaNegociosTotem.Modulo2;
+
+namespace PruebasUnitariasTotem.Modulo2
+{
+    /// <summary>
+    /// Prueba unitaria sobre la clase Logica Cliente
+    /// </summary>
+    [TestFixture]
+    public class PruebaLogicaContacto
+    {
+        LogicaContacto logicaContacto;
+        Contacto contacto;
+
+        /// <summary>
+        /// Método que inicializa 
+        /// la clase a probar
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            logicaContacto = new LogicaContacto();
+
+            Contacto contacto = new Contacto();
+        }
+
+
+        /// <summary>
+        /// Se prueba que la clase pueda agregar 
+        /// a los clientes jurídicos
+        /// </summary>
+        /// 
+        [Test]
+        public void PruebaConsultarContactoLogica()
+        {
+
+            string nombre = "Valentina";
+            string apellido = "Scioli";
+            int cedula = 11111111;
+
+
+           int  identificador = 6;
+            Assert.AreSame(logicaContacto.ConsultarDatosDeContacto(identificador).Con_Nombre, nombre);
+            Assert.AreSame(logicaContacto.ConsultarDatosDeContacto(identificador).Con_Apellido, apellido);
+            Assert.AreSame(logicaContacto.ConsultarDatosDeContacto(identificador).Con_Id, cedula);
+                 
+        
+        
+        
+        }
+
+ 
+
+         
+
+        /// <summary>
+        /// Método que vacía el objeto utilizado durante 
+        /// todas las pruebas para ser limpiado por el 
+        /// Recolector de Basura
+        /// </summary>
+        /// 
+        [TearDown]
+        public void Limpiar()
+        {
+            logicaContacto = null;
+        }
+
+    }
+}
