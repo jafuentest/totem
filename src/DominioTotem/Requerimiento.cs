836190554e156dfd8f9981f7eebea05a51febf24
Wolfgang Dielingen <wolfrainx@gmail.com>
2015-05-28 11:53:22 -0430

    Creación de Requerimiento en la capa Dominio
    - Creación de Requerimiento.cs
    - Actualización de DominioTotem.csproj

---
+++ src/DominioTotem/Requerimiento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DominioTotem
+{
+    class Requerimiento
+    {
+	   #region Atributos
+	   private string id;
+	   private string descripcion;
+	   private string tipo;
+	   private string prioridad;
+	   private string estatus;
+	   #endregion
+
+	   #region Propiedades
+	   public string Id
+	   {
+		  get { return id; }
+		  set { id = value; }
+	   }
+	   public string Descripcion
+	   {
+		  get { return descripcion; }
+		  set { descripcion = value; }
+	   }
+	   public string Tipo
+	   {
+		  get { return tipo; }
+		  set { tipo = value; }
+	   }
+	   public string Prioridad
+	   {
+		  get { return prioridad; }
+		  set { prioridad = value; }
+	   }
+	   public string Estatus
+	   {
+		  get { return estatus; }
+		  set { estatus = value; }
+	   } 
+	   #endregion
+    }
+}
