c23db3ab6e15d659aff55682eb3e103f938fa56b
tjsearsz <tjsearsz@gmail.com>
2015-05-28 20:44:34 -0430

    Inclusion de Modificacion Actor

--- src/Interfaz/src/GUI/Modulo6/ListarActores.aspx.cs
+++ src/Interfaz/src/GUI/Modulo6/ListarActores.aspx.cs
@@ -36,54 +36,17 @@ public partial class GUI_Modulo6_ListarActores : System.Web.UI.Page
         //Nos indica si hubo alguna accion de agregar, modificar o eliminar
         String success = Request.QueryString["success"];
         
-        //Declaramos una variable que almacenara el id del actor
+        //Declaramos una variable que almacenara el ID del actor
         int idActor = 0;
+
+        //Declaramos una variable que almacenara el ID del proyecto en String
+        String proyecto;
+
+        //Declaramos una variable que almacenara el ID del proyecto en INT
+        int proyectoID = 0;
 
         //Instanciamos la logica de los actores
         LogicaActor logica = new LogicaActor();
-
-        //Revisamos si es alguno de los casos a continuacion
-		switch (success)
-		{
-            //Si se viene de un agregar mostrara esta alerta
-			case "1":
-				alert.Attributes["class"] = "alert alert-success alert-dismissible";
-				alert.Attributes["role"] = "alert";
-				alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor agregado exitosamente</div>";
-				break;
-
-            //Si se viene de un modificar se procedera a este y mostrar la alerta correspondiente
-			case "2":
-				alert.Attributes["class"] = "alert alert-success alert-dismissible";
-				alert.Attributes["role"] = "alert";
-				alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor modificado exitosamente</div>";
-				break;
-
-            //Si se viene de un eliminar se procedera a eliminar y mostrar la alerta correspondiente
-			case "3":
-                //Casteamos explicitamente el ID del actor proveniente de un GET en la URL
-				idActor=Int32.Parse(Request.QueryString["id"]);
-
-                //Obtenemos el exito o fallo del proceso
-                bool exito= logica.EliminarActor(idActor,0);
-
-                //Evaluamos la condicion
-                if (exito)
-                {
-                    //Si la eliminacion fue exitosa mostramos esta alerta
-                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
-                    alert.Attributes["role"] = "alert";
-                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor eliminado exitosamente</div>";
-                }
-                else
-                {
-                    //Si fue fallida mostramos esta alerta
-                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
-                    alert.Attributes["role"] = "alert";
-                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Eliminacion fallida del actor</div>";
-                }
-				break;
-		}
 
         //Obtenemos la cookie que nos indicara el proyecto en el que nos encontramos
         HttpCookie projectCookie = Request.Cookies.Get("selectedProjectCookie");
@@ -92,12 +55,12 @@ public partial class GUI_Modulo6_ListarActores : System.Web.UI.Page
         if (projectCookie != null)
         {
             //Obtenemos el ID del proyecto en string
-            string proyecto = projectCookie.Values["projectCode"];
+            proyecto = projectCookie.Values["projectCode"];
 
             try
-            {   
+            {
                 //Casteamos explicitamente el ID del proyecto
-                int proyectoID = Int32.Parse(proyecto);
+                proyectoID = Int32.Parse(proyecto);
 
                 //Obtenemos todos los actores del proyecto
                 this.listaActores = logica.ListarActor(proyectoID);
@@ -118,5 +81,72 @@ public partial class GUI_Modulo6_ListarActores : System.Web.UI.Page
                 throw new FormatException("La cookie tiene un valor de proyecto no valido", exe);
             }
         }
+
+        //Revisamos si es alguno de los casos a continuacion
+		switch (success)
+		{
+            //Si se viene de un agregar mostrara esta alerta
+			case "1":
+				alert.Attributes["class"] = "alert alert-success alert-dismissible";
+				alert.Attributes["role"] = "alert";
+				alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor agregado exitosamente</div>";
+				break;
+
+            //Si se viene de un modificar se procedera a este y mostrar la alerta correspondiente
+			case "2":
+                //Casteamos explicitamente el ID del actor proveniente de un GET en la URL
+                idActor = Int32.Parse(Request.QueryString["id"]);
+
+                //Casteamos explicitamente el nombre del actor proveniente de un GET en la URL
+                String nombreActor = Request.QueryString["nombre"];
+
+                //Casteamos explicitamente la descripcion del actor proveniente de un GET en la URL
+                String descripcionActor = Request.QueryString["descripcion"];
+
+                bool exito2 = logica.ModificarActor(idActor, nombreActor, descripcionActor, proyectoID);
+
+                //Evaluamos la condicion
+                if (exito2)
+                {
+                    //Si la modificacion fue exitosa mostramos esta alerta
+                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                    alert.Attributes["role"] = "alert";
+                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor modificado exitosamente</div>";
+                }
+                else
+                {
+                    //Si la modificacion fue fallida mostramos esta alerta
+                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                    alert.Attributes["role"] = "alert";
+                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Modificacion fallida del actor</div>";
+                }
+                break;
+
+            //Si se viene de un eliminar se procedera a eliminar y mostrar la alerta correspondiente
+			case "3":
+                //Casteamos explicitamente el ID del actor proveniente de un GET en la URL
+				idActor=Int32.Parse(Request.QueryString["id"]);
+
+                //Obtenemos el exito o fallo del proceso
+                bool exito = logica.EliminarActor(idActor, proyectoID);
+
+                //Evaluamos la condicion
+                if (exito)
+                {
+                    //Si la eliminacion fue exitosa mostramos esta alerta
+                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                    alert.Attributes["role"] = "alert";
+                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Actor eliminado exitosamente</div>";
+                }
+                else
+                {
+                    //Si fue fallida mostramos esta alerta
+                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                    alert.Attributes["role"] = "alert";
+                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Eliminacion fallida del actor</div>";
+                }
+				break;
+		}
+        
 	}
 }
