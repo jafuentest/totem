54555162c93fdfd41130ac9e69a5a25b66493f98
Juan Fuentes <jn.fuentest@gmail.com>
2015-05-29 00:16:08 -0430

    update

---
+++ src/DominioTotem/CasoDeUso.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DominioTotem
+{
+    public class CasoDeUso
+    {
+        private int _idCasoUso;
+        private String _identificadorCasoUso;
+        private String _tituloCasoUso;
+        private List<String> _precondicionesCasoUso;
+        private String _condicionExito;
+        private String _condicionFallo;
+        private String _disparadorCasoUso;
+        private List<Dictionary<String, Dictionary<String, List<String>>>> _escenarioExito;
+
+        public CasoDeUso(int idCasoUso, String identificadorCasoUso, String tituloCasoUso,
+			List<String> precondicionesCasoUso, String condicionExito, String condicionFallo, String disparadorCasoUso,
+			List<Dictionary<String,Dictionary<String, List<String>>>> escenarioExito)
+		{
+			this._idCasoUso = idCasoUso;
+			this._identificadorCasoUso = identificadorCasoUso;
+			this._tituloCasoUso = tituloCasoUso;
+			this._precondicionesCasoUso = precondicionesCasoUso;
+			this._condicionExito = condicionExito;
+			this._condicionFallo = condicionFallo;
+			this._disparadorCasoUso = disparadorCasoUso;
+			this._escenarioExito = escenarioExito;
+        }
+
+        public int IdCasoUso
+        {
+            get { return _idCasoUso; }
+            set { _idCasoUso = value; }
+        }
+
+        public String IdentificadorCasoUso
+        {
+            get { return _identificadorCasoUso; }
+            set { _identificadorCasoUso = value; }
+        }
+
+        public String TituloCasoUso
+        {
+            get { return _tituloCasoUso; }
+            set { _tituloCasoUso = value; }
+        }
+
+        public List<String> PrecondicionesCasoUso
+        {
+            get { return _precondicionesCasoUso; }
+            set { _precondicionesCasoUso = value; }
+        }
+
+        public String CondicionExito
+        {
+            get { return _condicionExito; }
+            set { _condicionExito = value; }
+        }
+
+        public String CondicionFallo
+        {
+            get { return _condicionFallo; }
+            set { _condicionFallo = value; }
+        }
+
+        public String DisparadorCasoUso
+        {
+            get { return _disparadorCasoUso; }
+            set { _disparadorCasoUso = value; }
+        }
+
+        public List<Dictionary<String, Dictionary<String, List<String>>>> EscenarioExito
+        {
+            get { return _escenarioExito; }
+            set { _escenarioExito = value; }
+        }
+    }
+}
---
+++ src/LogicaNegociosTotem/Modulo6/LogicaCasoUso.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DominioTotem;
+
+namespace LogicaNegociosTotem.Modulo6
+{
+    class LogicaCasoUso
+    {
+        public List<CasoDeUso> ListarCasosDeUso()
+        {
+            int idCasoUso = 1;
+            String identificadorCasoUso = "TOT_CU_1_1_1";
+            String tituloCasoUso = "Iniciar Sesión";
+            
+			List<String> precondicionesCasoUso = new List<string>();
+			precondicionesCasoUso.Add("El usuario debe tener una cuenta iniciada");
+			precondicionesCasoUso.Add("El usuario debe tener su cuenta habilitada");
+
+            String condicionExito = "El usuario accede al sistema";
+            String condicionFallo = "El sistema vuelve a mostrar la pantalla de inicio de sesión";
+            String disparadorCasoUso = "El usuario intenta acceder a la aplicación";
+
+			List<String> pasosExtension1 = new List<String>();
+			pasosExtension1.Add("Se hace algo");
+			pasosExtension1.Add("Se hace otra cosa");
+
+			List<String> pasosExtension2 = new List<String>();
+			pasosExtension2.Add("Se hace algo distinta");
+			pasosExtension2.Add("Se hace otra cosa diferente");
+
+			Dictionary<string, List<string>> extensiones = new Dictionary<string, List<string>>();
+			extensiones.Add("El usuario cometió un error", pasosExtension1);
+			extensiones.Add("El usuario cometió un error grave", pasosExtension2);
+
+			Dictionary<String, Dictionary<String, List<String>>> paso1 =
+				new Dictionary<String, Dictionary<String, List<String>>>();
+			paso1.Add("El usuario ingresa al sistema", null);
+
+			Dictionary<String, Dictionary<String, List<String>>> paso2 =
+				new Dictionary<String, Dictionary<String, List<String>>>();
+			paso2.Add("El sistema despliega la pantalla de Inicio de Sesión",extensiones);
+			
+			List<Dictionary<String, Dictionary<String, List<String>>>> escenarioExito =
+				new List<Dictionary<String, Dictionary<String, List<String>>>>();
+			escenarioExito.Add(paso1);
+			escenarioExito.Add(paso2);
+
+			CasoDeUso casoDeUso = new CasoDeUso(idCasoUso, identificadorCasoUso, tituloCasoUso, precondicionesCasoUso,
+				condicionExito, condicionFallo, disparadorCasoUso, escenarioExito);
+
+			List<CasoDeUso> lista = new List<CasoDeUso>();
+			lista.Add(casoDeUso);
+
+			return lista;
+        }
+    }
+}
