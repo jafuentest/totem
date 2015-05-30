4e2a1eea2f2d01a0932e005875a4958c5ec8a6b7
Eduardo Pacheco <arzagoth@gmail.com>
2015-05-29 23:35:14 -0430

    Pruebas Unitarias
    pruebas

---
+++ src/PruebasUnitariasTotem/Modulo5/PruebaLogicaRequerimiento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using DominioTotem;
+using LogicaNegociosTotem.Modulo5;
+
+namespace PruebasUnitariasTotem.Modulo5
+{
+    [TestFixture]
+    class PruebaLogicaRequerimiento
+    {
+        private Proyecto elProyecto;
+        private Requerimiento elRequerimiento;
+
+        [SetUp]
+        public void init()
+        {
+
+            
+            elRequerimiento = new Requerimiento("test","test","test","test","test");
+                        
+
+        }
+        [TearDown]
+        public void clean()
+        {
+            elRequerimiento = null;
+        }
+
+        #region Crear
+       /* [Test]
+        public void PruebaCrearRequeriento()
+        {
+
+            try
+            {
+                Assert.IsTrue(LogicaNegociosTotem.Modulo5.LogicaRequerimiento.CrearRequerimiento(elRequerimiento));
+               
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+        */
+        #endregion
+
+        #region Consultar
+
+        /*  [Test]
+        public void PruebaConsultarRequerimiento()
+        {
+
+            Assert.IsTrue(LogicaNegociosTotem.Modulo5.LogicaRequerimiento.ConsultarRequerimiento(1).Count > 0);
+            
+
+        }
+        */
+        #endregion
+
+
+
+    }
+}
---
+++ src/PruebasUnitariasTotem/Modulo5/PruebaRequerimiento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DominioTotem;
+
+namespace PruebasUnitariasTotem.Modulo5
+{
+    [TestFixture]
+    class PruebaRequerimiento
+    {
+        private Requerimiento elRequerimiento;
+
+        [SetUp]
+        public void init()
+        {
+            elRequerimiento = new Requerimiento("TOT_RF_1", "Descripcion Requerimiento Funcional 1 Totem", "Funcional", "Alta", "Finalizado");
+        }
+        [TearDown]
+        public void clean()
+        {
+            elRequerimiento = null;
+        }
+
+        [Test]
+        public void pruebaReqNull()
+        {
+            Assert.IsNotNull(elRequerimiento);
+        }
+
+
+    }
+}
