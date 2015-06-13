a64a7282884295d2027df66e8871974520297a88
carlos lozano <aniolog@gmail.com>
2015-06-13 12:47:17 -0700

    presentadores

---
+++ src/Interfaz/src/Presentadores/Modulo5/PresentadorListarRequerimiento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contratos.Modulo5;
+
+namespace Presentadores.Modulo5
+{
+   /// <summary>
+   /// Presentadorla de vista ListarRequerimiento 
+   /// </summary>
+    public class PresentadorListarRequerimiento
+    {
+        /// <summary>
+        /// Vista asociada al presentador
+        /// </summary>
+        public IContratoListarRequerimientos vista;
+        /// <summary>
+        /// Constructor del presentador de la vista ListarContratos
+        /// </summary>
+        /// <param name="vista">vista la cual usuara el presentador</param>
+        public PresentadorListarRequerimiento(IContratoListarRequerimientos vista)
+        {
+            this.vista = vista;
+        }
+        /// <summary>
+        /// Metodo encargado de listar los requerimientos de un proyecto
+        /// </summary>
+        /// <param name="codigo">
+        /// Codigo del proyecto al cual se quieren obtener los requerimientos
+        /// </param>
+        public void ListarRequerimientosPorProyecto(String codigo) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Interfaz/src/Presentadores/Modulo5/PresentadorReportes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contratos.Modulo5;
+
+namespace Presentadores.Modulo5
+{
+  /// <summary>
+  /// Presentador de la vista de reportes
+  /// </summary>
+    public class PresentadorReportes
+    {
+        /// <summary>
+        /// Presentador de la vista Reportes
+        /// </summary>
+        private IContratoReportes vista;
+        /// <summary>
+        /// Contructor del presentador de la vista Reportes
+        /// </summary>
+        /// <param name="vista">vista la cual usuara el presentador</param>
+        public PresentadorReportes(IContratoReportes vista)
+        {
+            this.vista = vista;
+        }
+         
+
+    }
+}
