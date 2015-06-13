4d7202c25a10304322e8b27f5cd96392bc5e866c
carlos lozano <aniolog@gmail.com>
2015-06-13 12:45:17 -0700

    contratos

---
+++ src/Interfaz/src/Contratos/Modulo5/IContratoListarRequerimientos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DominioTotem;
+
+namespace Contratos.Modulo5
+{
+
+    /// <summary>
+    /// Contrato asociado a la vista ListarRequerimiento
+    /// </summary>
+    public interface IContratoListarRequerimientos
+    {
+    }
+}
---
+++ src/Interfaz/src/Contratos/Modulo5/IContratoReportes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contratos.Modulo5
+{
+    /// <summary>
+    /// Contrato asociado a la vista Reportes
+    /// </summary>
+    public interface IContratoReportes
+    {
+    }
+}
