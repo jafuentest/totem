9c4c4e407a4e088679e8875927201068f851ef17
samgville <sammyvillegas@gmail.com>
2015-06-13 15:05:19 -0430

    Implementacion Ejecutar ComandoModificarRequerimiento
    No estoy claro si esta bien el casteo de cosas

--- src/LogicaNegociosTotem/Comandos/Comandos/Modulo5/ComandoModificarRequerimiento.cs
+++ src/LogicaNegociosTotem/Comandos/Comandos/Modulo5/ComandoModificarRequerimiento.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DAO;
 
 namespace Comandos.Comandos.Modulo5
 {
@@ -14,7 +15,14 @@ namespace Comandos.Comandos.Modulo5
         /// <returns>true si se logro editar</returns>
         public override bool Ejecutar(Dominio.Entidad parametro)
         {
-            throw new NotImplementedException();
+
+            DAO.Fabrica.FabricaAbstractaDAO fabricaDaoSqlServer = DAO.Fabrica.FabricaDAOSqlServer.ObtenerFabricaSqlServer();
+            
+            DAO.DAO.Modulo5.DAORequerimiento daoRequerimiento = (DAO.DAO.Modulo5.DAORequerimiento)fabricaDaoSqlServer.ObtenerDAORequerimiento();
+           
+            return daoRequerimiento.Modificar(parametro);
+
+
         }
     }
 }
