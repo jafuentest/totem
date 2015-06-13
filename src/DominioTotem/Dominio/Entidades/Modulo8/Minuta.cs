057955c7a76e3228fb261b1ed73dbac3be55f97a
mgraterol <mgratero@gmail.com>
2015-06-13 17:57:21 -0430

    Actualizacion Entidades Modulo 8
    Creacion de Entidades Minuta, Acuerdo y Punto

---
+++ src/DominioTotem/Dominio/Entidades/Modulo8/Minuta.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dominio.Entidades.Modulo8
+{
+    /// <summary>
+    /// Clase del Dominio
+    /// </summary>
+    public class Minuta : Entidad
+    {
+        #region Atributos
+
+        private string codigo;
+        private DateTime fecha;
+        private string motivo;
+        private string observaciones;
+        /*private List<Usuario> usuario;
+        private List<Contacto> contacto;*/
+        private List<Punto> punto;
+        private List<Acuerdo> acuerdo;
+
+        #endregion
+
+        #region Constructores
+        public Minuta()
+        {
+        }
+
+        public Minuta(DateTime fecha, string motivo, string observaciones)
+        {
+            this.fecha = fecha;
+            this.motivo = motivo;
+            this.observaciones = observaciones;
+        }
+
+        public Minuta(string codigo, DateTime fecha, string motivo, string observaciones)
+        {
+            this.codigo = codigo;
+            this.fecha = fecha;
+            this.motivo = motivo;
+            this.observaciones = observaciones;
+        }
+
+        /*public Minuta(DateTime fecha, string motivo, string observaciones, List<Usuario> usuario,
+           List<Contacto> contacto, List<Punto> punto)
+        {
+            this.contacto = contacto;
+            this.fecha = fecha;
+            this.motivo = motivo;
+            this.observaciones = observaciones;
+            this.usuario = usuario;
+            this.punto = punto;
+        }
+
+        public Minuta(string codigo, DateTime fecha, string motivo, string observaciones, List<Usuario> usuario,
+            List<Contacto> contacto, List<Punto> punto, List<Acuerdo> acuerdo)
+        {
+            this.contacto = contacto;
+            this.codigo = codigo;
+            this.fecha = fecha;
+            this.motivo = motivo;
+            this.observaciones = observaciones;
+            this.usuario = usuario;
+            this.punto = punto;
+            this.acuerdo = acuerdo;
+        }*/
+
+        #endregion
+
+        #region Propiedades
+        public string Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return this.fecha; }
+            set { this.fecha = value; }
+        }
+
+        public string Motivo
+        {
+            get { return this.motivo; }
+            set { this.motivo = value; }
+        }
+
+        public string Observaciones
+        {
+            get { return this.observaciones; }
+            set { this.observaciones = value; }
+        }
+
+        /*public List<Usuario> ListaUsuario
+        {
+            get { return this.usuario; }
+            set { this.usuario = value; }
+        }
+
+        public List<Contacto> ListaContacto
+        {
+            get { return this.contacto; }
+            set { this.contacto = value; }
+        }*/
+
+        public List<Punto> ListaPunto
+        {
+            get { return this.punto; }
+            set { this.punto = value; }
+        }
+
+        public List<Acuerdo> ListaAcuerdo
+        {
+            get { return this.acuerdo; }
+            set { this.acuerdo = value; }
+        }
+        #endregion
+    }
+}
---
+++ src/DominioTotem/Dominio/Entidades/Modulo8/Punto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dominio.Entidades.Modulo8
+{
+    /// <summary>
+    /// Clase del Dominio
+    /// </summary>
+    public class Punto : Entidad
+    {
+        #region Atributos
+
+        private int codigo;
+        private string titulo;
+        private string desarrollo;
+
+        #endregion
+
+        #region Constructores
+
+        public Punto()
+        {
+        }
+
+        public Punto(int codigo, string titulo, string desarrollo)
+        {
+            this.codigo = codigo;
+            this.titulo = titulo;
+            this.desarrollo = desarrollo;
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        public int Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+
+        public string Titulo
+        {
+            get { return this.titulo; }
+            set { this.titulo = value; }
+        }
+
+        public string Desarrollo
+        {
+            get { return this.desarrollo; }
+            set { this.desarrollo = value; }
+        }
+
+        #endregion
+    }
+}
