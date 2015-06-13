f787736f920394a972b5be6dbc9dc26167edc14f
croix000 <plasenciajohan@hotmail.com>
2015-06-13 16:28:03 -0430

    DAO Minutas

---
+++ src/DatosTotem/DAO/DAO/Modulo8/DaoPunto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAO.IntefazDAO;
+using Dominio;
+using Dominio.Fabrica;
+using Dominio.Entidades.Modulo2;
+using System.Data;
+
+namespace DAO.DAO.Modulo8
+{
+    public class DaoPunto : DAO, IntefazDAO.Modulo8.IDaoPunto
+    {
+        public bool Agregar(Entidad parametro)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Modificar(Entidad parametro)
+        {
+            throw new NotImplementedException();
+        }
+        public Entidad ConsultarXId(Entidad parametro)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Entidad> ConsultarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ src/DatosTotem/DAO/IntefazDAO/Modulo8/IDaoMinuta.cs
@@ -0,0 +1,12 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAO.IntefazDAO.Modulo8
+{
+    public interface IDaoMinuta : IDao<Entidad, bool, Entidad>
+    {
+    }
+}
