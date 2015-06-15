d62c26fcb7d63d1844eb77cee33ec4828efee0ff
tjsearsz <tjsearsz@gmail.com>
2015-06-14 21:19:04 -0430

    Registrar usuario en DAO

---
+++ src/DatosTotem/DAO/DAO/Modulo7/DAOUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAO.IntefazDAO.Modulo7;
+using Dominio.Entidades.Modulo7;
+
+namespace DAO.DAO.Modulo7
+{
+    /// <summary>
+    /// Clase DAO que interactua con la BD y realiza las operaciones del Usuario
+    /// </summary>
+    public class DAOUsuario : IDaoUsuario
+    {
+        /// <summary>
+        /// Metodo que agrega un Usuario nuevo a la Base de Datos
+        /// </summary>
+        /// <param name="usuario">El usuario a ser insertado en la BD</param>
+        /// <returns>Verdadero si el usuario fue agregado o falso sino fue agregado</returns>
+        public bool AgregarUsuario(Usuario usuario)
+        {
+            return false;
+        }
+        
+    }
+}
---
+++ src/DatosTotem/DAO/IntefazDAO/Modulo7/IDaoUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dominio.Entidades.Modulo7;
+
+namespace DAO.IntefazDAO.Modulo7
+{
+    /// <summary>
+    /// Interface a ser usada por la clase DAOUsuario
+    /// </summary>
+    interface IDaoUsuario
+    {
+        /// <summary>
+        /// Metodo que implementara la logica del agregar usuario a la Base de Datos
+        /// </summary>
+        /// <param name="usuario">El usuario a ser agregado</param>
+        /// <returns>Verdadero si fue agregado o falso sino fue agregado</returns>
+        public bool AgregarUsuario(Usuario usuario);
+        
+    }
+}
