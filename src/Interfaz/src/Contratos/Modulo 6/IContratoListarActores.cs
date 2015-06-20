3635399ea5b16430b56a687f977825a794cfc064
Minervavr <minerva_veru@hotmail.com>
2015-06-20 11:44:40 -0430

    Contrato de la interfaz para consultar todos los actores

---
+++ src/Interfaz/src/Contratos/Modulo 6/IContratoListarActores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contratos.Modulo6
+{
+    public interface IContratoListarActores
+    {
+        string laTabla { get; set; }
+        string actor_id { get; set; }
+        string actor_nombre { get; set; }
+        string actor_descripcion { get; set; }
+    }
+}
