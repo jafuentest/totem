87c0e1f32bfbe0329f950912fe8081b1e7f44d63
Minervavr <minerva_veru@hotmail.com>
2015-06-20 11:38:43 -0430

    Excepciones en Comando Consultar Actores

---
+++ src/ExcepcionesTotem/Modulo6/ExcepcionesComando/ConsultarActoresComandoNullException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo6.ExcepcionesComando
+{
+    public class ConsultarActoresComandoNullException : ExcepcionesTotem.ExceptionTotem
+    {
+        public ConsultarActoresComandoNullException()
+            : base()
+        { }
+
+        public ConsultarActoresComandoNullException(string message)
+            : base(message)
+        {
+        }
+
+        public ConsultarActoresComandoNullException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public ConsultarActoresComandoNullException(string codigo, string message, Exception inner)
+            : base(codigo, message, inner)
+        {
+        }
+    }
+}
---
+++ src/ExcepcionesTotem/Modulo6/ExcepcionesComando/ConsultarTodosActoresNuloDAOException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcepcionesTotem.Modulo6.ExcepcionesComando
+{
+    public class ConsultarTodosActoresNuloDAOException : ExcepcionesTotem.ExceptionTotem
+    {
+        public ConsultarTodosActoresNuloDAOException()
+            : base()
+        { }
+
+        public ConsultarTodosActoresNuloDAOException(string message)
+            : base(message)
+        {
+        }
+
+        public ConsultarTodosActoresNuloDAOException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public ConsultarTodosActoresNuloDAOException(string codigo, string message, Exception inner)
+            : base(codigo, message, inner)
+        {
+        }
+    }
+}
