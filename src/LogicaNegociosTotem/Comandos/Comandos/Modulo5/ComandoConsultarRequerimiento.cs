e9402591dc12fa825104f1ac13b107c3d91ffa3c
sbernal93 <santiagobernal93@gmail.com>
2015-06-21 12:49:40 -0430

    Comando consultar requerimiento

--- src/LogicaNegociosTotem/Comandos/Comandos/Modulo5/ComandoConsultarRequerimiento.cs
+++ src/LogicaNegociosTotem/Comandos/Comandos/Modulo5/ComandoConsultarRequerimiento.cs
@@ -15,7 +15,34 @@ namespace Comandos.Comandos.Modulo5
         /// <returns>Requerimiento con los datos cargados</returns>
         public override Dominio.Entidad Ejecutar(Dominio.Entidad parametro)
         {
-            throw new NotImplementedException();
+            DAO.Fabrica.FabricaAbstractaDAO fabricaDAO;
+            DAO.IntefazDAO.Modulo5.IDaoRequerimiento daoRequerimiento;
+
+            fabricaDAO = DAO.Fabrica.FabricaAbstractaDAO.ObtenerFabricaSqlServer();
+            daoRequerimiento = fabricaDAO.ObtenerDAORequerimiento();
+
+            try
+            {
+                return daoRequerimiento.ConsultarXId(parametro);
+            }
+
+            #region Capturar Excepciones
+            catch (ExcepcionesTotem.Modulo1.ParametroInvalidoException ex)
+            {
+
+                throw ex;
+            }
+            catch (ExcepcionesTotem.ExceptionTotemConexionBD ex)
+            {
+
+                throw ex;
+            }
+            catch (ExcepcionesTotem.Modulo5.RequerimientoInvalidoException ex)
+            {
+
+                throw ex;
+            }
+            #endregion
         }
     }
 }
