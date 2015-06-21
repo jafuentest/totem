666c5b2136fd2ca851299b83dfe6b4f3fc482920
mapl93 <mapl93@hotmail.com>
2015-06-20 22:12:23 -0430

    Comando M8

---
+++ src/LogicaNegociosTotem/Comandos/Comandos/Modulo8/ComandoListaMinuta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcepcionesTotem;
+using ExcepcionesTotem.Modulo8.ExcepcionesDeDatos;
+using Dominio;
+using DAO.Fabrica;
+using DAO.DAO.Modulo8;
+using System.Data.SqlClient;
+
+namespace Comandos.Comandos.Modulo8
+{
+    class ComandoListaMinuta : Comando<String,List<Dominio.Entidad>>
+    {
+
+        public override List<Dominio.Entidad> Ejecutar(String parametro)
+        {
+            try
+            {
+                FabricaAbstractaDAO fabricaDAO = FabricaAbstractaDAO.ObtenerFabricaSqlServer();
+                DAO.IntefazDAO.Modulo8.IDaoMinuta daoMinuta = fabricaDAO.ObtenerDAOMinuta();
+                return daoMinuta.ConsultarMinutasProyecto(parametro);
+            }
+            catch (NullReferenceException ex)
+            {
+                throw ex;
+            }
+            catch (ExcepcionesTotem.ExceptionTotemConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (ParametroIncorrectoException ex)
+            {
+                throw ex;
+            }
+            catch (AtributoIncorrectoException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
