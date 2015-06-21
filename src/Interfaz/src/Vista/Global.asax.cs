5e05d7f3edc6c23d67620aa3ffdf92799fd43770
sbernal93 <santiagobernal93@gmail.com>
2015-06-20 21:33:34 -0430

    Prevencion XXScripting

---
+++ src/Interfaz/src/Vista/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Vista.Modulo5
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+            var exception = context.Server.GetLastError();
+            if (exception is HttpRequestValidationException)
+            {
+                
+                Response.Redirect(Request.Url.AbsolutePath + "?error=input_malicioso");
+                return;
+            }
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
