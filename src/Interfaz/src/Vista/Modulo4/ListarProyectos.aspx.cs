06f919a4ec16536ce21fc112aab0f8a33cad5e9c
Wolfgang Dielingen <wolfrainx@gmail.com>
2015-06-24 15:43:27 -0430

    Arreglos en el PresentadorListarProyectos y ListarProyectos
    - Arreglos en los enlaces del MasterPage para ListarProyectos.
    - Arreglos en el menú lateral para contener las opciones necesarias.
    - Integración de los archivos .csproj

---
+++ src/Interfaz/src/Vista/Modulo4/ListarProyectos.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vista.Modulo4
+{
+    public partial class ListarProyectos : System.Web.UI.Page,
+	   Contratos.Modulo4.IContratoListarProyectos
+    {
+
+	   private Presentadores.Modulo4.PresentadorListarProyectos presentador;
+
+	   public ListarProyectos()
+	   {
+		  presentador = new Presentadores.Modulo4.PresentadorListarProyectos(this);
+	   }
+
+	   protected void Page_Load(object sender, EventArgs e)
+	   {
+		  if ( !this.IsPostBack )
+		  {
+			 this.Master.idModulo = "4";
+			 this.Master.presentador.CargarMenuLateral();
+		  }
+	   }
+
+	   public string codigoProyecto
+	   {
+		  get { throw new NotImplementedException(); }
+	   }
+    }
+}
