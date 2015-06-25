d4106f01764ac95c495a3347d44881c3b2c165f9
sbernal93 <santiagobernal93@gmail.com>
2015-06-25 17:06:08 -0430

    Consultar Codigo de Requerimiento

--- script/ElScript.sql
+++ script/ElScript.sql
@@ -2177,6 +2177,26 @@ AS
 GO
 
 -- ========================================================================= --
+-- Retornar último cdigo de requerimiento por proyecto
+-- ========================================================================= --
+
+CREATE PROCEDURE M5_RetornarCodigoDeRequerimiento
+
+  @pro_id       [int] ,
+  @req_tipo     [varchar] (25) ,
+  @req_codigo     [varchar] (15)  OUTPUT
+
+AS
+  BEGIN
+    SELECT @req_codigo = req_codigo
+    FROM REQUERIMIENTO R
+    WHERE ( R.PROYECTO_pro_id = @pro_id
+        AND LOWER(R.req_tipo) = LOWER(@req_tipo) )
+    ORDER BY req_codigo;
+  END
+GO
+
+-- ========================================================================= --
 -- Eliminar requerimiento
 -- ========================================================================= --
 CREATE PROCEDURE M5_EliminarRequerimiento
