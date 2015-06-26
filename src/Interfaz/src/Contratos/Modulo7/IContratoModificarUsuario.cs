9bd1d6659930184e1c8cf2cf2dc6bc254ad5eea7
Juan Fuentes <jn.fuentest@gmail.com>
2015-06-26 19:29:14 -0430

    Modificar usuario presentador

--- src/Interfaz/src/Contratos/Modulo7/IContratoModificarUsuario.cs
+++ src/Interfaz/src/Contratos/Modulo7/IContratoModificarUsuario.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Data;
 
 namespace Contratos.Modulo7
 {
+	/// <summary>
+	/// Contrato asociado a la vista Modificar Usuario
+	/// </summary>
 	public interface IContratoModificarUsuario
 	{
+		string Nombre { get; set; }
+		string Apellido { get; set; }
+		string Username { get; set; }
+		string Correo { get; set; }
+		string Clave { get; set; }
+		string ConfirmarClave { get; set; }
+		string Rol { get; set; }
+		string Cargo { get; set; }
+		string Pregunta { get; set; }
+		string Respuesta { get; set; }
+		DataSet Roles { get; set; }
+		DataSet Cargos { get; set; }
 	}
 }
