16827aaf78bae3e699c2ae44c814b3b924372f6a
carlos lozano <aniolog@gmail.com>
2015-06-26 17:10:09 -0700

    Pruebas unitarias de comando y fabrica (Modulo 5)

---
+++ src/PruebasUnitariasTotem/Modulo5/PruebaComandoEliminarRequerimiento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PruebasUnitariasTotem.Modulo5
+{
+    class PruebaComandoEliminarRequerimiento
+    {
+        private Comandos.Comando<Dominio.Entidad, Boolean> comando;
+        private Dominio.Entidades.Modulo5.Requerimiento requerimiento;
+
+        /// <summary>
+        /// Inicializacion de atributos
+        /// </summary>
+        [SetUp]
+        public void init()
+        {
+            comando = Comandos.Fabrica.FabricaComandos.CrearComandoEliminarRequerimiento();
+            Dominio.Fabrica.FabricaEntidades fabrica = new Dominio.Fabrica.FabricaEntidades();
+            this.requerimiento = fabrica.ObtenerRequerimiento() as Dominio.Entidades.Modulo5.Requerimiento;
+            this.requerimiento.Codigo = "FB_RF_2";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void PruebaEjecutar()
+        {
+            try
+            {
+                this.comando.Ejecutar(this.requerimiento);
+            }
+            catch (ExcepcionesTotem.Modulo1.ParametroInvalidoException err)
+            {
+            }
+            catch (ExcepcionesTotem.ExceptionTotemConexionBD)
+            {
+            }
+            catch (ExcepcionesTotem.Modulo5.RequerimientoInvalidoException)
+            {
+
+            }
+
+        }
+
+
+    }
+}
